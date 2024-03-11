5d183db29d90867c046919c1c4b427f265632c04
MhmmdIlyas <160574547+MhmmdIlyas@users.noreply.github.com>
2024-03-12 05:34:14 +0800

    Add files via upload

---
+++ Tugas3/Source Code/Tugas14.java
@@ -0,0 +1,21 @@
+// 13020220137, M Ilyas Reskiawan In, Senin 11 Maret 2024
+import java.util.Scanner;
+/* Baca N, */
+/* Print i = 1 s/d N dengan while (ringkas) */
+public class Tugas14 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+// TODO Auto-generated method stub
+        /* Kamus : */
+        int N;
+        int i = 1;
+        Scanner masukan=new Scanner(System.in);
+        /* Program */
+        System.out.print ("Nilai N >0 = "); N = masukan.nextInt(); System.out.print ("Print i dengan WHILE (ringkas): \n");
+        while (i <= N){
+            System.out.println (i++);
+        } /* (i > N)*/
+    }
+}
---
+++ Tugas3/Source Code/Tugas2.java
@@ -0,0 +1,14 @@
+// 13020220137, M Ilyas Reskiawan In, Senin 11 Maret 2024
+public class Tugas2 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+// TODO Auto-generated method stub
+        /* Program */
+        System.out.println("Program akan looping, akhiri dengan ^c");
+        while (true)
+        { System.out.print ("Print satu baris.....\n");
+        }
+    }
+}
